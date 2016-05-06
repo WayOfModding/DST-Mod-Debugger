93a629e4b88f500b5a4fb850232e007c1cc78bad
Nymphoenix <donizyo@gmail.com>
2016-05-06 16:38:34 +0800

    Add modinfo.lua

---
+++ modinfo.lua
@@ -0,0 +1,33 @@
+--The name of the mod displayed in the 'mods' screen.
+name = "Don't Starve Together Debugger"
+
+--A description of the mod.
+description = "This mod will process crash report and interpret which mod is to blame.\n\nThis mod is created after I wrote a guide named [url=http://steamcommunity.com/sharedfiles/filedetails/?id=670337421]How to understand crash reports[/url]. Visit the guide to know more about how this mod works."
+description_cn = "帮助你理解错误报告。\n\n这个mod是我在撰写了一篇名为《[url=http://steamcommunity.com/sharedfiles/filedetails/?id=670337421]怎样让错误报告说人话[/url]》的指南之后制作的。访问指南以了解本mod的工作原理。"
+
+--Who wrote this awesome mod?
+author = "Nymphoenix"
+
+--A version number so you can ask people if they are running an old version of your mod.
+version = "0.1"
+
+--This lets other players know if your mod is out of date.  This typically needs to be updated every time there's a new game update.
+api_version = 10
+
+-- Compatible with Don't Starve Together
+dst_compatible = true
+
+-- Compatible with both the base game and reign of giants
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+
+--Some mods may crash or not work correctly until the game is restarted after the mod is enabled/disabled
+restart_required = false
+
+-- Set this to true to prevent _ANY_ other mods from loading while this mod is enabled.
+standalone = false
+
+--icon_atlas = "screechericon.xml"
+--icon = "screechericon.tex"
+
+forumthread = ""
